1bb424ec8acccf383230d84ffe51c7866b2f8c93
Khimya <khimyakhetarpal@gmail.com>
2014-05-29 17:09:02 +0530

    latest update

--- problem3.py
+++ problem3.py
@@ -1,19 +1,18 @@
 #What is the largest prime factor of the number 600851475143 ?
 
-
+import math
 import time
 start =  time.time()
-num = 13195
-#num = 600851475143
-#all = xrange(1,num+1)
+#num = 13195
+num = 600851475143
+
 factors = []
 largest = []
-for i in xrange(1,num+1):
+for i in range(1,num+1):
     if (num%i == 0):
         factors.append(i)
 
 #print factors
-length = len(factors)        
 #print length
 
 length = len(factors)
@@ -24,12 +23,12 @@ while j < length and j >= 0 :
     count = 0
     num  = factors[j]
     #print num 
-    for k in xrange(2,num):
+    for k in range(2,num):
         if (num%k == 0):
            count = 1 
     #print count
     if (count == 0):
-        print num
+        print(num)
         break
     j = j - 1
       
