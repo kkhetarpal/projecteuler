8a376ea2bfaf8a665d127ee35d478a203f88140d
Khimya <khimyakhetarpal@gmail.com>
2014-05-29 16:46:08 +0530

    Largest Prime Factor

--- problem3.py
+++ problem3.py
@@ -4,31 +4,33 @@
 import time
 start =  time.time()
 num = 13195
-all = range(1,num+1)
+#num = 600851475143
+#all = xrange(1,num+1)
 factors = []
 largest = []
-for i in all:
+for i in xrange(1,num+1):
     if (num%i == 0):
         factors.append(i)
 
-print factors
+#print factors
 length = len(factors)        
-print length
+#print length
+
+length = len(factors)
+j = length-1
 
 
-#numm = []
-# numm.append(factors[15])
-# print numm
-
-
-for j in range(length-1,1):
-    numm = factors[j]
-    print numm
-    for k in range(2,numm-1):
-        if (numm%2 == 0):
-            j = j - 1
-        else:
-            print numm
-        
-          
+while j < length and j >= 0 :
+    count = 0
+    num  = factors[j]
+    #print num 
+    for k in xrange(2,num):
+        if (num%k == 0):
+           count = 1 
+    #print count
+    if (count == 0):
+        print num
+        break
+    j = j - 1
+      
 #print time.time() - start
