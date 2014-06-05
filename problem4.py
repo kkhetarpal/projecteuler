7ce5b69f830709be3cb38e749436d44ca973f7e9
Khimya <khimyakhetarpal@gmail.com>
2014-06-05 20:06:22 +0530

    Largest palindrome product

---
+++ problem4.py
@@ -0,0 +1,31 @@
+def palindrome(num):
+    original = num
+    reversed = 0
+    
+    if num < 10: return 1
+    if num % 10 == 0: return 0
+
+    while num >= 1:
+        reversed = (reversed * 10) + (num % 10) 
+        num = num / 10
+    
+    if original == reversed: return 1
+    else: return 0
+
+# number = 1114
+# print palindrome(number)
+
+i = 999
+j = 999
+check = 0
+while i >= 99:
+    product = 0
+    while j >= 99:
+        product = i * j
+        print i,j,product
+        check = palindrome(product)
+        if check == 1:
+            print product
+            exit
+        j = j -1
+    i = i - 1
